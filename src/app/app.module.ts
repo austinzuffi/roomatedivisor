bb73efaa420a584e0a9664fc62529eeaa4eaac99
austinzuffi <austinzuffi@gmail.com>
2018-07-02 08:37:31 -0700

    github push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
-import {MatIconModule, MatButtonModule} from '@angular/material';
+import { MatIconModule, 
+        MatButtonModule,
+        MatInputModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -19,9 +22,11 @@ import { MainSqueezeComponent } from './main-squeeze/main-squeeze.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase, 'roommate-divisor'),
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
     HttpClientModule
   ],
   providers: [AngularFirestore, AngularFireAuth],
